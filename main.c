e9ef23e259bb9bc7fc720b519706566b5d22a31d
Juris Ormanis <ormanis1609@gmail.com>
2023-12-21 08:15:35 +0000

    deleted some lines of hello world

--- main.c
+++ main.c
@@ -5,8 +5,5 @@ int main()
     printf("Hello World!!\n");
     printf("Hello World!!\n");
     printf("Hello World!!\n");
-    printf("Hello World!!\n");
-    printf("Hello World!!\n");
-    printf("Hello World!!\n");
     return 0;
 }
